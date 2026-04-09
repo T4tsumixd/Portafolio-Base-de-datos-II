2b0701001229ecf6f07e0fc01dab2bb708eb4a91
T4tsumixd <cubajavier33@gmail.com>
2026-04-08 20:20:32 -0500

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+function scrollToTrabajos() {
+  document.getElementById("trabajos").scrollIntoView({
+    behavior: "smooth"
+  });
+}
+
+function verProyecto() {
+  alert("Aquí puedes poner el link de tu proyecto");
+}
